b442457c662a27d0ea0d511099a0b160a2d0bce2
raulherediatorres <120607232+raulherediatorres@users.noreply.github.com>
2023-11-03 16:52:06 +0100

    Add files via upload

---
+++ examen_herediatorresraul/main.js
@@ -0,0 +1,35 @@
+const jsonSillas =
+[
+    {
+      "nombre": "Silla de Comedor",
+      "material": "Madera",
+      "color": "Blanco",
+      "estilo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9iVX2cO8_FxierK8buz-Dh1yuPtJ6AMfnDgBx0rlimA&s",
+      "precio": 150.00,
+      "disponibilidad": true
+    },
+    {
+      "nombre": "Silla de Oficina",
+      "material": "Metal y Cuero",
+      "color": "Negro",
+      "estilo": "Ejecutivo",
+      "precio": 250.00,
+      "disponibilidad": true
+    },
+    {
+      "nombre": "Silla de Jardín",
+      "material": "Plástico",
+      "color": "Verde",
+      "estilo": "Exterior",
+      "precio": 50.00,
+      "disponibilidad": false
+    },
+    {
+      "nombre": "Silla de Bar",
+      "material": "Acero Inoxidable",
+      "color": "Plateado",
+      "estilo": "Contemporáneo",
+      "precio": 80.00,
+      "disponibilidad": true
+    }
+  ]
